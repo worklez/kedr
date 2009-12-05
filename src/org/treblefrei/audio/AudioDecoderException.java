4ded9a4e6853dbf2aa2475304e18d8a8de4156c3
Gleb Kanterov <gleb@kanterov.ru>
2009-12-05 19:35:57 +0000

    Fixed AudioDecoder behavior, now works fine

---
+++ src/org/treblefrei/audio/AudioDecoderException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.treblefrei.audio;
+
+/**
+ *
+ * @author gleb
+ */
+public class AudioDecoderException extends Exception {
+
+}
