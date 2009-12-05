facc3abb6e53e74527a83c03bcaeebdeabab0159
Ilya Kuznetsov <worklez@gmail.com>
2009-12-05 20:25:47 +0000

    ofa seems to be working

--- src/org/treblefrei/ofa/Ofa.java
+++ src/org/treblefrei/ofa/Ofa.java
@@ -1,7 +1,13 @@
 package org.treblefrei.ofa;
 
+import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
+import org.treblefrei.audio.AudioDecoder;
+import org.treblefrei.audio.AudioDecoderException;
+import org.treblefrei.audio.DecodedAudioData;
+
+import java.io.FileNotFoundException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,11 +16,24 @@ import com.sun.jna.NativeLong;
  * Time: 23:53:37
  */
 
+interface COfa extends Library {
+    COfa instance = (COfa) Native.loadLibrary("ofa", COfa.class);
+    String ofa_create_print(byte[] samples, int byteOrder, NativeLong size, int sRate, int stereo);
+}
+
 public class Ofa {
     static String createPrint(byte[] samples, int sRate, int stereo) {
-        COfa cofa = (COfa) Native.loadLibrary("ofa", COfa.class);
         NativeLong nlSize = new NativeLong(samples.length);
         // LITTLE_ENDIAN = 0, BIG_ENDIAN = 1
-        return cofa.ofa_create_print(samples, 0, nlSize, sRate, stereo);
+        return COfa.instance.ofa_create_print(samples, 0, nlSize, sRate, stereo);
+    }
+
+    static String createPrint(String filename) throws AudioDecoderException, FileNotFoundException {
+            DecodedAudioData data = AudioDecoder.getSamples(filename, 135);
+            int channels = data.getChannels();
+            if (channels > 2) {
+                throw new RuntimeException("too many audio channels");
+            }
+            return Ofa.createPrint(data.getData(), data.getSampleRate(), channels - 1);
     }
 }
--- src/org/treblefrei/ofa/TestMain.java
+++ src/org/treblefrei/ofa/TestMain.java
@@ -1,5 +1,12 @@
 package org.treblefrei.ofa;
 
+import org.treblefrei.audio.AudioDecoderException;
+import org.treblefrei.audio.AudioDecoder;
+
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by IntelliJ IDEA.
  * User: worklez
@@ -9,6 +16,13 @@ package org.treblefrei.ofa;
 
 public class TestMain {
     public static void main(String args[]) {
-        //Ofa.createPrint();
+        try {
+            System.out.println(Ofa.createPrint("1.flac"));
+            System.out.println(Ofa.createPrint("2.mp3"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AudioDecoder.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (AudioDecoderException ex) {
+            Logger.getLogger(AudioDecoder.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
