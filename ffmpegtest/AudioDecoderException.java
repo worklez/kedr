d75f0ac9ac22ac636b550c2ed5f520ede0350ff3
Gleb Kanterov <gleb@kanterov.ru>
2009-12-05 17:09:38 +0000

    Added AudioDecoder implementation

---
+++ ffmpegtest/AudioDecoderException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ffmpegtest;
+
+/**
+ *
+ * @author gleb
+ */
+public class AudioDecoderException extends Exception {
+
+}
