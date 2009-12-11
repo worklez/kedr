c643ff7550a61f85e62a754e3f2535c0ea1180c1
Ilya Kuznetsov <worklez@gmail.com>
2009-12-11 10:22:36 +0000

    About dialog set to modal

--- src/org/treblefrei/kedr/gui/qt/QKedrAboutDialog.java
+++ src/org/treblefrei/kedr/gui/qt/QKedrAboutDialog.java
@@ -1,13 +1,13 @@
 package org.treblefrei.kedr.gui.qt;
 
 import com.trolltech.qt.gui.QDialog;
-import com.trolltech.qt.gui.QMainWindow;
 import com.trolltech.qt.gui.QWidget;
 
 public class QKedrAboutDialog extends QDialog {
 
     public QKedrAboutDialog(QWidget parent) {
         super(parent);
+        this.setModal(true);
     }
 }
  
