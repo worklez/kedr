af8f92102aef8d9cc749291b09a210b0572d9452
Gleb Kanterov <gleb@kanterov.ru>
2009-12-13 18:13:24 +0000

    Added translation support to about dialog

--- src/org/treblefrei/kedr/gui/qt/QKedrAboutDialog.java
+++ src/org/treblefrei/kedr/gui/qt/QKedrAboutDialog.java
@@ -7,10 +7,10 @@ public class QKedrAboutDialog extends QDialog {
     public QKedrAboutDialog(QWidget parent) {
         super(parent);
         setModal(true);
-        setWindowTitle("About");
+        setWindowTitle(tr("About"));
         QVBoxLayout layout = new QVBoxLayout();
-        QLabel about = new QLabel("KEDR работает на электрон-позитронном коллайдере ВЭПП-4М", this);
-        QPushButton okButton = new QPushButton("OK", this);
+        QLabel about = new QLabel(tr("KEDR работает на электрон-позитронном коллайдере ВЭПП-4М"), this);
+        QPushButton okButton = new QPushButton(tr("OK"), this);
         layout.addWidget(about);
         layout.addWidget(okButton);
         setLayout(layout);
